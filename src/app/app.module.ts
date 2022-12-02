c86a539055b9512c2cb85f35b63745aa7d1faf56
Lucas Marchant <lucascardosomarchant@gmail.com>
2022-12-01 23:08:45 -0300

    crinado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CardModule} from 'primeng/card';
+import { LoginComponent } from './views/login/login.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    CardModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { RequestLogin } from 'src/app/resources/models/RequestLogin';
+import { LoginService } from 'src/app/resources/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit  {
+
+  public requestLogin: RequestLogin = new RequestLogin;
+
+  constructor(private loginService: LoginService) {
+
+  }
+
+  ngOnInit(): void {
+    this.requestLogin = new RequestLogin();
+  }
+
+  public doLongin() :void {
+    this.loginService.doLogin(this.requestLogin).subscribe(data => {
+      console.log(data);
+    },
+    error => {
+     console.error(error); 
+    })
+  }
+
+}
+
+
